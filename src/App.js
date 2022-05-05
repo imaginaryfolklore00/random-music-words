9aa9bb24342a929d9b08f1124f027e5e52415e62
Nazeem <nazeem.s2k@gmail.com>
2022-05-05 14:25:50 +0200

    Added user input and configured lint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <h1>Random words into music titles!</h1>
+  const [inputNumber, setInputNumber] = useState(5);
+
+  const handleNumberSubmit = (event) => event.preventDefault();
+
+  //This is a form to get number input from the user, the input is kept in a state 'inputNumber'
+  const numberForm = () => (
+    <form onSubmit={handleNumberSubmit}>
+      <div>
+        Number of words:
+        <input
+          id="inputNumber"
+          type="number"
+          value={inputNumber}
+          name="NumberOfWords"
+          min="5"
+          max="20"
+          onChange={({ target }) => setInputNumber(target.value)}
+        />
+      </div>
+      <button id="number-button" type="submit">
+        Go
+      </button>
+    </form>
   );
-}
+
+  return (
+    <>
+      <h1>Random words into music titles</h1>
+      {numberForm()}
+    </>
+  );
+};
 
 export default App;
